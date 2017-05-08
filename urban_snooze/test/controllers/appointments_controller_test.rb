9d618a3d0390f92c9a03b47ccde5ca9cfe2df8e8
Matt <matt.fasano88@gmail>
2017-05-08 15:32:26 -0400

    added_appointments

---
+++ urban_snooze/test/controllers/appointments_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class AppointmentsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get appointments_index_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get appointments_create_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get appointments_new_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get appointments_show_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get appointments_destroy_url
+    assert_response :success
+  end
+
+end
