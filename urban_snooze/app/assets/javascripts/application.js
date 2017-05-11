be2e2e7c50768ff129b72a1e22bfe45ebdd6be70
Joel Varughese <jtvarughese@gmail.com>
2017-05-11 16:33:35 -0400

    js map functioning

--- urban_snooze/app/assets/javascripts/application.js
+++ urban_snooze/app/assets/javascripts/application.js
@@ -15,14 +15,13 @@
 //= require turbolinks
 //= require_tree .
 
-
 var map;
-  function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 40.7831, lng: -73.9712},
-      zoom: 11
-    });
-  }
+  // function initMap() {
+  //   map = new google.maps.Map(document.getElementById('map'), {
+  //     center: {lat: 40.7831, lng: -73.9712},
+  //     zoom: 11
+  //   });
+  // }
 
 // Map Display
 function initMap() {
@@ -40,44 +39,44 @@ function initMap() {
 
   // Array of locations, which are looped to display multiple markers
   var locations = [
-    ['Tribeca',  40.7163, -74.0086, 1],
-    ['Soho', 40.7233, -74.0030, 2],
-    ['Fidi', 40.7075, -74.0113, 3],
-    ['Times Square', 40.7589, -73.9851, 4],
-    ['Union Square', 40.7359, -73.9911, 5],
-    ['Midtown', 40.7484, -73.9857, 6]
+    ['Urban Snooze Tribeca',  40.7163, -74.0086, 1],
+    ['Urban Snooze Soho', 40.7233, -74.0030, 2],
+    ['Urban Snooze Fidi', 40.7075, -74.0113, 3],
+    ['Urban Snooze Times Square', 40.7589, -73.9851, 4],
+    ['Urban Snooze Union Square', 40.7359, -73.9911, 5],
+    ['Urban Snooze Midtown', 40.7484, -73.9857, 6]
   ];
   // Info window content
-  var infoWindowContent = [
-    ['<div class="info_content">' +
-    '<h3>Urban Snooze Tribeca</h3>' + '</div>'],
-    ['<div class="info_content">' +
-    '<h3>Urban Snooze Soho</h3>' +
-    '</div>'],
-    ['<div class="info_content">' +
-    '<h3>Urban Snooze Fidi</h3>' +
-    '</div>'],
-    ['<div class="info_content">' +
-    '<h3>Urban Snooze Times Square</h3>' +
-    '</div>'],
-    ['<div class="info_content">' +
-    '<h3>Urban Snooze Union Square</h3>' +
-    '</div>']
-];
+  // var infoWindowContent = [
+  //   ['<div class="info_content">' +
+  //   '<h3>Urban Snooze Tribeca</h3>' + '</div>'],
+  //   ['<div class="info_content">' +
+  //   '<h3>Urban Snooze Soho</h3>' +
+  //   '</div>'],
+  //   ['<div class="info_content">' +
+  //   '<h3>Urban Snooze Fidi</h3>' +
+  //   '</div>'],
+  //   ['<div class="info_content">' +
+  //   '<h3>Urban Snooze Times Square</h3>' +
+  //   '</div>'],
+  //   ['<div class="info_content">' +
+  //   '<h3>Urban Snooze Union Square</h3>' +
+  //   '</div>']
+  // ];
 
   // Add multiple markers to map
-  var infoWindow = new google.maps.InfoWindow(), marker, i;
-
+  var infoWindow = new google.maps.InfoWindow();
+    var marker, i
     for (i = 0; i < locations.length; i++) {
     marker = new google.maps.Marker({
       position: new google.maps.LatLng(locations[i][1], locations[i][2]),
       map: map
     });
+    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+      return function() {
+        infoWindow.setContent(locations[i][0]);
+        infoWindow.open(map, marker);
+      }
+    })(marker, i));
   }
-  google.maps.event.addEventListener(marker, 'click', (function(marker, i) {
-    return function() {
-      infoWindow.setContent(infoWindowContent[i][0]);
-      infoWindow.open(map, marker);
-    }
-  })(marker, i));
 }
