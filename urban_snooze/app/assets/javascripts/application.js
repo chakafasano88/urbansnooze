e9785efcb87a6eee1dafcac6ca716824d8368356
Matt <matt.fasano88@gmail>
2017-05-12 09:04:01 -0400

    update

--- urban_snooze/app/assets/javascripts/application.js
+++ urban_snooze/app/assets/javascripts/application.js
@@ -16,67 +16,54 @@
 //= require_tree .
 
 var map;
-  // function initMap() {
-  //   map = new google.maps.Map(document.getElementById(‘map’), {
-  //     center: {lat: 40.7831, lng: -73.9712},
-  //     zoom: 11
-  //   });
-  // }
-
 // Map Display
 function initMap() {
 //  Tribeca parameters
   var nyc = {lat: 40.7359, lng: -73.9911};
-  var map = new google.maps.Map(document.getElementById(‘map’), {
+  var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 12,
     center: nyc
   });
 // Tribeca marker
   var marker = new google.maps.Marker({
-    position: nyc,
     map: map
   });
 
  // Array of locations, which are looped to display multiple markers
   var locations = [
-    [‘Urban Snooze Tribeca’,  40.7163, -74.0086, 1],
-    [‘Urban Snooze Soho’, 40.7233, -74.0030, 2],
-    [‘Urban Snooze Fidi’, 40.7075, -74.0113, 3],
-    [‘Urban Snooze Times Square’, 40.7589, -73.9851, 4],
-    [‘Urban Snooze Union Square’, 40.7359, -73.9911, 5],
-    [‘Urban Snooze Midtown’, 40.7484, -73.9857, 6]
+    ["Urban Snooze Tribeca",  40.7163, -74.0086, 1],
+    ["Urban Snooze Soho", 40.7233, -74.0030, 2],
+    ["Urban Snooze Fidi", 40.7075, -74.0113, 3],
+    ["Urban Snooze Times Square", 40.7589, -73.9851, 4],
+    ["Urban Snooze Union Square", 40.7359, -73.9911, 5],
+    ["Urban Snooze Midtown", 40.7484, -73.9857, 6]
   ];
-  // Info window content
-  // var infoWindowContent = [
-  //   [‘<div class=“info_content”>’ +
-  //   ‘<h3>Urban Snooze Tribeca</h3>’ + ‘</div>‘],
-  //   [‘<div class=“info_content”>’ +
-  //   ‘<h3>Urban Snooze Soho</h3>’ +
-  //   ‘</div>‘],
-  //   [‘<div class=“info_content”>’ +
-  //   ‘<h3>Urban Snooze Fidi</h3>’ +
-  //   ‘</div>‘],
-  //   [‘<div class=“info_content”>’ +
-  //   ‘<h3>Urban Snooze Times Square</h3>’ +
-  //   ‘</div>‘],
-  //   [‘<div class=“info_content”>’ +
-  //   ‘<h3>Urban Snooze Union Square</h3>’ +
-  //   ‘</div>‘]
-  // ];
 
- // Add multiple markers to map
+  // Drop down list array
+  var locationChoice = document.getElementById('location_choice')
+
+  marker.setAttribute('style', '');
+
+// Adds onlick INFO WINDOW to marker
   var infoWindow = new google.maps.InfoWindow();
+
+   // Adds multiple MARKERS to map
     var marker, i
     for (i = 0; i < locations.length; i++) {
     marker = new google.maps.Marker({
       position: new google.maps.LatLng(locations[i][1], locations[i][2]),
       map: map
     });
-    google.maps.event.addListener(marker, ‘click’, (function(marker, i) {
+    // Adds onlick INFO WINDOW to marker
+    google.maps.event.addListener(marker, 'click', (function(marker, i) {
       return function() {
         infoWindow.setContent(locations[i][0]);
         infoWindow.open(map, marker);
       }
     })(marker, i));
+    locationChoice.querySelector('onclick', function(event) {
+        addMarker(event.latLng);
+      });
+    // Loop ends
   }
 }
